5ab518ce70ea0a55ff743cc7cf49ebea7be2b255
Vyacheslav Fedin <fedinvv1978@gmail.com>
2022-08-20 17:35:24 +1000

    001

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//поиск наибольшего числа из двух введеных
+Console.WriteLine("Введите число 1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int b = int.Parse(Console.ReadLine());
+if (a > b) 
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
+else 
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+}
